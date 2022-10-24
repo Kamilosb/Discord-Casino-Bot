b85b6ea34082f04cb59d45d79c46153af2be7649
Kamilosb <Kamilosb.kk@gmail.com>
2022-10-24 21:00:48 +0200

    patch na ruletce

--- Commands/Economy/roulette.js
+++ Commands/Economy/roulette.js
@@ -5,15 +5,7 @@ const Database = require("../../Schemas/purse");
 //Black roulette numbers: 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, and 35.
 //jak w zegarze 0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26.
 
-function getRandom(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
 
-function delay(n){ // w sekundach
-    return new Promise(function(resolve){
-        setTimeout(resolve,n*1000)
-    })
-}
 
 
 module.exports = {
@@ -38,7 +30,17 @@ module.exports = {
         .setRequired(true)
     ),
 
-    async execute(interaction) {  
+    async execute(interaction) { 
+        function getRandom(min, max) { // min and max included 
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+        
+        function delay(n){ // w sekundach
+            return new Promise(function(resolve){
+                setTimeout(resolve,n*1000)
+            })
+        }
+        
         const { options } = interaction
         const bid = parseInt(options.getNumber("kwota"))
         let userData = await Database.findOne({User: interaction.user.id})
