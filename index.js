7d2ef09936b9d7d997e484d47f8a7302765184dc
Kamilosb <Kamilosb@duck.com>
2022-11-04 12:14:47 +0100

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember } = Partials;
 require("dotenv").config()
 
+//teasdasdas
 const client = new Client({ 
     intents: [Guilds, GuildMembers, GuildMessages],
     partials: [User, Message, GuildMember, ThreadMember]
